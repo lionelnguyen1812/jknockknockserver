2e9136921e7234e15d7d9e2594db5bac3609aa57
Mr. Fukugami Makoto <lionel.nguyen1812@gmail.com>
2013-08-17 04:14:24 +0700

    chờ server gửi pcket

--- knockknockserver/src/knockknockserver/KnockKnockServer.java
+++ knockknockserver/src/knockknockserver/KnockKnockServer.java
@@ -1,10 +1,16 @@
 /* @author chad */
 package knockknockserver;
 
+import java.net.SocketException;
+
 public class KnockKnockServer {
     
     public static void main(String[] args) {
-        
+        try {
+            new QuoteServerThread().start();
+        } catch (SocketException ex) {
+            System.out.println("cannot start quote thread");
+        }
     }
 
 }

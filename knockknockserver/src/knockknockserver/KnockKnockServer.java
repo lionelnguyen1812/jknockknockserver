818636f9e0049e92d0807666047505549f5cc404
Mr. Fukugami Makoto <lionel.nguyen1812@gmail.com>
2013-08-17 09:32:05 +0700

    comit

--- knockknockserver/src/knockknockserver/KnockKnockServer.java
+++ knockknockserver/src/knockknockserver/KnockKnockServer.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /* @author chad */
 package knockknockserver;
 
@@ -15,3 +16,22 @@ public class KnockKnockServer {
     }
 
 }
+=======
+/* @author chad */
+package knockknockserver;
+
+import java.net.SocketException;
+
+public class KnockKnockServer {
+    // :) co gang len
+    // mes 1
+    public static void main(String[] args) {
+        try {
+            new QuoteServerThread().start();
+        } catch (SocketException ex) {
+            System.out.println("cannot start quote thread");
+        }
+    }
+
+}
+>>>>>>> f25d4ec29fb526380c92a9f5079705168c278427

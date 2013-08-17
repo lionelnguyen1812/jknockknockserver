cbe4989e787e82ea32d49762ced1258b8f24a55f
HungChilvas <hunglm_b01501@fpt.aptech.ac.vn>
2013-08-17 13:15:36 +0700

    Revert "delele"
    This reverts commit 83ca6c078ca45c71bb8ac4d270f649b34cb17fbf.

---
+++ knockknockserver/src/knockknockserver/NewClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package knockknockserver;
+
+/**
+ *
+ * @author caubuonviai1803
+ */
+public class NewClass {
+    int i = 0;
+    int k = 0;
+    
+}

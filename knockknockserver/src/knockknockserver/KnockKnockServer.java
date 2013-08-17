fc46fbc4923118908cabae3e869a7af5ee00fbd0
Mr. Fukugami Makoto <lionel.nguyen1812@gmail.com>
2013-08-18 01:19:52 +0700

    received OK

--- knockknockserver/src/knockknockserver/KnockKnockServer.java
+++ knockknockserver/src/knockknockserver/KnockKnockServer.java
@@ -1,20 +1,26 @@
 /* @author chad */
 package knockknockserver;
 
+import java.io.IOException;
 import java.net.DatagramSocket;
 
 public class KnockKnockServer {
 
     private static final int LOCAL_PORT = 8888;
-    
+    private SocketService ss;
     ServerLogger logger;
 
     public KnockKnockServer() {
         logger = new ServerLogger("KnockKnockServer");
+        ss = new SocketService();
     }
     
+    public void startSocketService() throws IOException{
+        ss.serve(LOCAL_PORT);
+    }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         KnockKnockServer server = new KnockKnockServer();
+        server.startSocketService();
     }
 }
--- knockknockserver/src/knockknockserver/SocketService.java
+++ knockknockserver/src/knockknockserver/SocketService.java
@@ -8,6 +8,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 class SocketService {
+
     private Thread serveThread = null;
     ServerLogger logger;
     DatagramSocket socket = null;
@@ -16,11 +17,6 @@ class SocketService {
     public SocketService() {
         this.logger = new ServerLogger("SocketService");
     }
-    
-    public static void main(String[] args) throws IOException {
-        SocketService ss = new SocketService();
-        ss.serve(8888);
-    }
 
     void serve(int port) throws IOException {
         socket = new DatagramSocket(port);
@@ -28,25 +24,32 @@ class SocketService {
             @Override
             public void run() {
                 while (true) {
-                    try {
-                        byte[] buf = new byte[4096];
-                        DatagramPacket packet = new DatagramPacket(buf, buf.length);
-                        logger.log("waiting for new packet");
-                        socket.receive(packet);
-                        logger.log("received a packet");
-                        new Thread(new Responder(socket, packet)).start();
-                        connections++;
-                    } catch (IOException ex) {
-                        Logger.getLogger(SocketService.class.getName()).log(Level.SEVERE, null, ex);
-                    }
+                    listenToNextPacket();
                 }
             }
         });
         serveThread.start();
     }
 
+    private void listenToNextPacket() {
+        try {
+            byte[] buf = new byte[4096];
+            DatagramPacket packet = new DatagramPacket(buf, buf.length);
+            logger.log("waiting for new packet");
+            socket.receive(packet);
+            logger.log("received a packet");
+            new Thread(new Responder(socket, packet)).start();
+            connections++;
+        } catch (IOException ex) {
+            Logger.getLogger(SocketService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void close() {
+        socket.close();
+    }
+
     public int connections() {
         return connections;
     }
-
 }

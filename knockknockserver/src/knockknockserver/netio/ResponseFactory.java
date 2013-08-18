b3b84db0345d6ccb26001151eaa4d775cff10495
Mr. Fukugami Makoto <lionel.nguyen1812@gmail.com>
2013-08-18 15:51:57 +0700

    commit

--- knockknockserver/src/knockknockserver/netio/ResponseFactory.java
+++ knockknockserver/src/knockknockserver/netio/ResponseFactory.java
@@ -20,24 +20,35 @@ public class ResponseFactory implements Runnable {
         this.logger = new ServerLogger("Responder Logger");
     }
 
-    private byte[] makeResponse() {
+    public String responToLoginRequest(String userID, String encryptedPassword){
+        String response = "";
+        
+        
+        return response;
+    }
+    
+    private String makeResponse() {
         String response = "";
         String[] splitedDataPacket = new String(this.packet.getData()).split(";");
         switch (splitedDataPacket[0]) {
             case "login":
+                String userID = splitedDataPacket[1];
+                String encryptedPassword = splitedDataPacket[2];
+                response = responToLoginRequest(userID, encryptedPassword);
                 break;
             default:
                 break;
         }
-        return response.getBytes();
+        return response;
     }
 
     @Override
     public void run() {
-        byte[] dataForResponPacket = makeResponse();
-        DatagramPacket respsonsePacket = new DatagramPacket(dataForResponPacket, dataForResponPacket.length, packet.getAddress(), packet.getPort());
+        String dataForResponPacket = makeResponse();
+        byte[] buf = dataForResponPacket.getBytes();
+        DatagramPacket respsonsePacket = new DatagramPacket(buf, buf.length, packet.getAddress(), packet.getPort());
         try {
-            socket.send(packet);
+            socket.send(respsonsePacket);
         } catch (IOException ex) {
             logger.log("could not send response");
         }
